10f69e78937102545781fddb29631ae505a295f2
Marguerite Blair <marguerite.blair@gmail.com>
2020-06-23 12:41:40 -0400

    function to iterate through objects

---
+++ countup.js
@@ -0,0 +1,10 @@
+function getCount(objects) {
+    let count = 0;
+    for (let i = 0; i < objects.length; i++) {
+        if (objects[i].x ===  objects[i].y) {
+            count++;
+        }
+    }
+    return count;
+    
+}
